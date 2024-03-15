824cb3145ba6c79a98e92c509f5f2d65ec8a3826
AsadCh001 <137525360+AsadCh001@users.noreply.github.com>
2024-03-15 11:31:17 +0500

    onboarding

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,11 +1,45 @@
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default function BottomBar(){
-    return(
-        <>
-        <main>
-            <h1>bottom bar</h1>
-        </main>
-        </>
-    )
+import { sidebarLinks } from "@/constants";
+
+function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={16}
+                height={16}
+                className='object-contain'
+              />
+
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
+
+export default Bottombar;
