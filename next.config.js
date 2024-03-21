04e525d48b2745dd9c126042efad9f4f5c83e946
AsadCh001 <137525360+AsadCh001@users.noreply.github.com>
2024-03-21 10:46:58 +0500

    tsignore

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: true
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
